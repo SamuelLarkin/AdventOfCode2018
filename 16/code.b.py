35e6837023e9b110795d07360d89edc297ecb2d0
Samuel Larkin <Samuel.Larkin@cnrc-nrc.gc.ca>
2018-12-17 06:45:01 -0500

    Another algorithm for perfect matching that is not peeling.

--- 16/code.b.py
+++ 16/code.b.py
@@ -13,25 +13,50 @@ if __name__ == '__main__':
     possible_opcodes = parse_data()
 
     # [Matching (graph theory)](https://en.wikipedia.org/wiki/Matching_(graph_theory))
+    # [](https://www.reddit.com/r/adventofcode/comments/a6p5ih/day_16_part_2_solving_opcodes/ebwsivn)
     if False:
-        ass = [-1] * len(ops) # their number -> my number
+        import random
+        import sys
+        op_id_to_name = opcodes.keys()
+        print('op_id_to_name:', op_id_to_name)
+        op_name_to_id = { op_name: i for i, op_name in enumerate(op_id_to_name) }
+        print('op_name_to_id:', op_name_to_id)
+        edges = [ { op_name_to_id[n] for n in names } for names in possible_opcodes.values() ]
+        print('edges:', edges)
+        ass = [-1] * len(opcodes) # their number -> my number
         def assign(v):
-            import random
-            w = random.choice(edges[v])
+            w = random.choice(list(edges[v]))
             u, ass[w] = ass[w], v
             if u != -1: assign(u)
-        for i in range(len(ops)):
+        for i in range(len(opcodes)):
             assign(i)
+        print(ass)
+        sys.exit(0)
+        # [11, 6, 0, 8, 4, 7, 10, 12, 3, 2, 5, 1, 15, 13, 9, 14]
 
-    if False:
-        op_name_to_id = { op_name: i for i, op_name in possible_opcodes.keys() }
-        edges = [ { op_name_to_id[n] } for names in possible_opcodes.values() ]
+    if True:
+        # This is a better method since the previous method hits a maximum recursion limit.
         import random
-        ass = [-1]*len(ops) # their number -> my number
-        for v in range(len(ops)):
+        import sys
+        op_id_to_name = opcodes.keys()
+        print('op_id_to_name:', op_id_to_name)
+        op_name_to_id = { op_name: i for i, op_name in enumerate(op_id_to_name) }
+        print('op_name_to_id:', op_name_to_id)
+        edges = [ [ op_name_to_id[n] for n in names ] for names in possible_opcodes.values() ]
+        print('edges:', edges)
+        import random
+        ass = [-1] * len(opcodes) # their number -> my number
+        for v in range(len(opcodes)):
             while v != -1:
                 w = random.choice(edges[v])
                 v, ass[w] = ass[w], v
+        print(ass)
+        import sys
+        sys.exit(0)
+        # opcodes number read from the data.txt file is match to my opcodes which are in opcodes.opcodes
+        # opcode 0 is match to my opcode 11
+        # opcode 5 is match to my opcode 7
+        # [11, 6, 0, 8, 4, 7, 10, 12, 3, 2, 5, 1, 15, 13, 9, 14]
 
 
     op_codes_by_id = [None] * 16
